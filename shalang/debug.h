784969179b63bc3d3a480f2208e0309482e4aac6
Sam <asamadans@gmail.com>
2024-07-04 18:46:36 +0530

    shalang/debug.*: simple disassembler

---
+++ shalang/debug.h
@@ -0,0 +1,9 @@
+#ifndef cshalang_debug_h
+#define cshalang_debug_h
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif
